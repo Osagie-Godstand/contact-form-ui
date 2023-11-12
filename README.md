6d14cf688bda306d2f02c3397175dfa80a37fd20
Osagie-Godstand <51753700+Osagie-Godstand@users.noreply.github.com>
2023-11-12 02:40:35 +0000

    contact-form-ui

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # contact-form-ui
 Enabling inquiries
+
+## Contact Form Submission with Node.js and Express
+This project demonstrates a simple form submission using Node.js and Express. The HTML form is styled with CSS, and the server-side logic is handled by Express to process the form data.
+
+## Prerequisites
+Make sure you have Node.js installed on your machine. You can download it from [https://nodejs.org/](https://nodejs.org/).
+
+
+
+
---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+const port = 3000;
+
+// Middleware
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('src'));
+
+// Handling form submission
+app.post('/submit_form', (req, res) => {
+  const { name, email, phonenumber, subject } = req.body;
+  // Processing the form data as needed
+  console.log('Form data:', { name, email, phonenumber, subject });
+  // Responding to the client
+  res.send('Form submitted successfully!');
+});
+
+// Serving an HTML file for the root path
+app.get('/', (_req, res) => {
+  res.sendFile(__dirname + '/src/index.html');
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server listening at http://localhost:${port}`);
+});
