581fc9f27c2d325c4e7f710af72dd6f86ae9d58a
Osagie-Godstand <51753700+Osagie-Godstand@users.noreply.github.com>
2023-11-19 20:35:47 +0000

    feat: error handling added.

--- server.js
+++ server.js
@@ -1,29 +1,51 @@
+require('dotenv').config();
+
 const express = require('express');
 const bodyParser = require('body-parser');
+const fs = require('fs');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000; // Use the environment variable or default to 3000
 
-// Middleware
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('src'));
 
+const logError = (error) => {
+  const timestamp = new Date().toISOString();
+  const logMessage = `[${timestamp}] ${error.stack}\n`;
+
+  console.error(logMessage);
+
+  fs.appendFile('errors.log', logMessage, (err) => {
+    if (err) {
+      console.error('Error writing to the error log:', err);
+    }
+  });
+};
+
 // Handling form submission
 app.post('/submit_form', (req, res) => {
-  const { name, email, phonenumber, message } = req.body;
-  // Processing the form data as needed
-  console.log('Form data:', { name, email, phonenumber, message });
-  // Responding to the client
-  res.send('Form submitted successfully!');
+  try {
+    const { name, email, phonenumber, message } = req.body;
+    // Processing the form data as needed
+    console.log('Form data:', { name, email, phonenumber, message });
+    // Responding to the client
+    res.send('Form submitted successfully!');
+  } catch (error) {
+    logError(error);
+    res.status(500).send('Internal Server Error');
+  }
 });
 
-// Serving an HTML file for the root path
 app.get('/', (_req, res) => {
-  res.sendFile(__dirname + '/src/index.html');
+  try {
+    res.sendFile(__dirname + '/src/index.html');
+  } catch (error) {
+    logError(error);
+    res.status(500).send('Internal Server Error');
+  }
 });
 
-// Start the server
 app.listen(port, () => {
   console.log(`Server listening at http://localhost:${port}`);
 });
-
